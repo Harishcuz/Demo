5e0579f1b0c1c0bb190b940815d7780469f646e5
Harishcuz <harishkumarchandrasekaraan@gmail.com>
2024-05-08 22:04:35 +0530

    master

---
+++ src/Java/Demo.java
@@ -0,0 +1,162 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Demo {
+		    public static void main(String[] args) {
+				/*
+				 * String Original = "MOM"; String reversed = reverseString(Original); //
+				 * System.out.println("Original: " + original); System.out.println("Reversed: "
+				 * + reversed); }
+				 * 
+				 * public static String reverseString(String str) { // Initialize an empty
+				 * string to store the reversed string String reversed = "";
+				 * 
+				 * // Iterate through the original string in reverse order using a for loop for
+				 * (int i = str.length() - 1; i >= 0; i--) { // Append each character to the
+				 * reversed string reversed += str.charAt(i); }
+				 * 
+				 * // Return the reversed string return reversed;
+				 */
+		
+		    	
+				/*
+				 * Scanner Scanner = new Scanner(System.in);
+				 * 
+				 * System.out.println(" hi whts your name ");
+				 * 
+				 * String name = Scanner.nextLine();
+				 * 
+				 * System.out.println("hi " + name +", thats a good  name   ");
+				 * 
+				 * 
+				 * System.out.println("whts your age ");
+				 * 
+				 * int age = Scanner.nextInt(); Scanner.nextLine(); System.out.println("great "
+				 * + age +", you're young enough for leanring java   ");
+				 * 
+				 * 
+				 * System.out.println("whts your email "); String email = Scanner.nextLine();
+				 * 
+				 * System.out.println(email);
+				 */
+		     
+				/*
+				 * int hk= 44 ;
+				 * 
+				 * System.out.println(hk<<1);
+				 * 
+				 * 
+				 * int a =20 ;
+				 * 
+				 * System.out.println((a<2) || (a>30) );
+				 * 
+				 * 
+				 * 
+				 * 
+				 */
+		  /*  	
+		    	int a =5;
+		    	int b = 5;
+		    	
+		    	
+		    	
+		    	Scanner Scanner = new Scanner(System.in);
+		    	
+		    	System.out.println("enter a value");
+		    	
+		    String name = Scanner.nextLine();
+		    
+		    System.out.println(name + a*b);
+		    
+		   char hk ;
+		    
+		   System.out.println("enter a sign");
+		   
+		  hk = Scanner.next().charAt(0);
+		  
+		  switch (hk) {
+		  case '+':
+			  System.out.println("good");
+		  
+		  */
+		  
+			/*
+			 * int marks[] = {30, 40 ,50, 60};
+			 * 
+			 * System.out.println(marks[2]);
+			 */
+		    	
+		    	
+	/*	    	int hk = 1;
+		    	Scanner Scanner =  new Scanner(System.in);
+		    	System.out.println("How many stars you want??");
+		    	int num = Scanner.nextInt();
+		    
+		   while(hk<=num)  	{  
+			   
+      	System.out.print("*");
+			   
+			   
+			   hk++; */
+		    	
+		  
+		    
+	/*
+	 * for(int i =1; i <=10; i++ ) {
+	 * 
+	 * System.out.print("*");
+	 * 
+	 * 
+	 * }
+	 */
+		   
+	  int n ;
+	  
+	  Scanner scanner = new Scanner(System.in);
+	    System.out.println("enter a num:");
+    	 n = scanner.nextInt() ;
+    	 
+    	 while(0<=n) {
+    	 if(n%10==0){
+    		 
+    		 System.out.println("correct");
+    	 }
+    	 }
+    		 
+    		 n++;
+    	 
+		        	
+		   
+		    	
+		  
+ 
+		    
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+}
+		    	
+		    	
+		    	
+}    	
+
+
+		    
+		    
+		    
+
+		    
+		
+	
+		
+
+			
